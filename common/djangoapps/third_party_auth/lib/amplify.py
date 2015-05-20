cc14ae434dc1d6c56d01af42e16956fc145bf292
Ting Li <tli@amplify.com>
2015-05-20 16:10:32 -0400

    Add amplify as third party auth provider for edX/Amplify SSO

---
+++ common/djangoapps/third_party_auth/lib/amplify.py
@@ -0,0 +1,38 @@
+"""
+Amplify OAuth2 Sign-in backends
+"""
+
+from social.backends.oauth import BaseOAuth2
+import json
+import urllib2
+from urllib import urlencode
+
+
+class AmplifyOAuth2(BaseOAuth2):
+    """Amplify OAuth authentication backend"""
+    name = 'amplify-oauth2'
+    REDIRECT_STATE = False
+    AUTHORIZATION_URL = 'http://tmc241.mc.wgenhq.net/mobilelogin/oauth2/auth'
+    ACCESS_TOKEN_URL = 'http://tmc241.mc.wgenhq.net/mobilelogin/oauth2/token'
+    ACCESS_TOKEN_METHOD = 'POST'
+    EXTRA_DATA = [
+        ('refresh_token', 'refresh_token', True),
+        ('expires_in', 'expires'),
+        ('token_type', 'token_type', True)
+    ]
+
+    def get_user_details(self, response):
+        """XXX, Return user details from Amplify account"""
+        return {'username': response.get('user_uid', ''),
+                'email': response.get('email', ''),
+                'fullname': response.get('name', '')}
+
+    def user_data(self, access_token, *args, **kwargs):
+        """Loads user data from service"""
+        opener = urllib2.build_opener()
+        opener.addheaders.append(('Cookie', 'sso.auth_token='+access_token))
+        url = opener.open("http://tmc241.mc.wgenhq.net/mobilelogin/gatekeeper")
+        try:
+            return json.load(url)
+        except ValueError:
+            return None
