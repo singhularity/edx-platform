953a4ce45e0b4940b469303398854218920dcb07
Ting Li <tli@amplify.com>
2015-05-27 16:13:20 -0400

    change amplify oauth class to fix the link and redirect_uri problems

--- common/djangoapps/third_party_auth/lib/amplify.py
+++ common/djangoapps/third_party_auth/lib/amplify.py
@@ -20,6 +20,21 @@ class AmplifyOAuth2(BaseOAuth2):
         ('token_type', 'token_type', True)
     ]
 
+    def get_user_id(self, details, response):
+        """Use user uid as unique id"""
+        return response['user_uid']
+
+    def auth_params(self, state=None):
+        client_id, client_secret = self.get_key_and_secret()
+        params = {
+            'client_id': client_id
+        }
+        if self.STATE_PARAMETER and state:
+            params['state'] = state
+        if self.RESPONSE_TYPE:
+            params['response_type'] = self.RESPONSE_TYPE
+        return params
+
     def get_user_details(self, response):
         """Return user details from Amplify account"""
         return {'username': response.get('user_uid', ''),
