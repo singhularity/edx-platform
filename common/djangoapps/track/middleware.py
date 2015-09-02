5e7a0aaf06e562914d70b088230e04464693a6be
aamir-khan <aamir@edx.org>
2015-09-02 18:20:11 +0500

    ECOM-2150: Added the ga client fetching as a customer header in middleware

--- common/djangoapps/track/middleware.py
+++ common/djangoapps/track/middleware.py
@@ -145,7 +145,11 @@ class TrackMiddleware(object):
         # this: _ga=GA1.2.1033501218.1368477899. The clientId is this part: 1033501218.1368477899.
         google_analytics_cookie = request.COOKIES.get('_ga')
         if google_analytics_cookie is None:
-            context['client_id'] = None
+            google_analytics_cookie = request.META.get('HTTP_X_EDX_GA_CLIENT_ID')
+            if google_analytics_cookie:
+                context['client_id'] = '.'.join(google_analytics_cookie.split('.'))
+            else:
+                context['client_id'] = None
         else:
             context['client_id'] = '.'.join(google_analytics_cookie.split('.')[2:])
 
