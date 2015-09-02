6189b13b9e38e6cb841068abb9688ebcbaa4b80a
aamir-khan <aamir@edx.org>
2015-09-02 18:52:24 +0500

    ECOM-2150: Updatde the code

--- common/djangoapps/track/middleware.py
+++ common/djangoapps/track/middleware.py
@@ -145,11 +145,7 @@ class TrackMiddleware(object):
         # this: _ga=GA1.2.1033501218.1368477899. The clientId is this part: 1033501218.1368477899.
         google_analytics_cookie = request.COOKIES.get('_ga')
         if google_analytics_cookie is None:
-            google_analytics_cookie = request.META.get('HTTP_X_EDX_GA_CLIENT_ID')
-            if google_analytics_cookie:
-                context['client_id'] = google_analytics_cookie
-            else:
-                context['client_id'] = None
+            context['client_id'] = request.META.get('HTTP_X_EDX_GA_CLIENT_ID')
         else:
             context['client_id'] = '.'.join(google_analytics_cookie.split('.')[2:])
 
